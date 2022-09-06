accd1e4220a9976bd2135d59e2a2786de48002e8
Nastya Spasskaya <Nastya312217@gmail.com>
2022-09-06 21:49:05 +0700

    Решение задачи 4 к семинару9

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу вычесления функции Аккермана
+
+int Akkerman(int M, int N)
+{
+    if (M == 0) return N + 1;
+    else 
+    {
+        if (N == 0 && M > 0) return Akkerman(M - 1, 1);
+        else return Akkerman(M-1, Akkerman(M, N-1));
+    }
+
+}
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Функция Аккtрмана от {M} и {N} равна {Akkerman(M, N)}");
